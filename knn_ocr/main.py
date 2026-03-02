d6abf845fbb39fb1d0161f3e77ce365fdf7bd449
Dest1n640 <realmadridfunca@gmail.com>
2026-03-02 21:39:16 +0800

    knn_ocr

--- knn_ocr/main.py
+++ knn_ocr/main.py
@@ -19,11 +19,11 @@ def extractor(image):
   lb = label(binary)
   props = regionprops(lb)
 
-
   features = [props[0].eccentricity,
               props[0].solidity,
               props[0].extent,
-              props[0].euler_number]
+              props[0].euler_number,
+              *props[0].moments_hu]
 
   return np.array(features, dtype="f4")
 
@@ -33,22 +33,20 @@ def make_train(path):
   responses = []
   ncls = 0
   for cls in sorted(path.glob("*")):
-#     print(cls)
     ncls += 1
     if cls.name[0] == "s":
       symbols[ncls] = cls.name[1]
     else:
       symbols[ncls] = cls.name
     for p in cls.glob("*.png"):
-#       print(p)
       train.append(extractor(imread(p)))
       responses.append(ncls)
-  train = np.array(train, dtype = "f4").reshape(-1, 4)
+  train = np.array(train, dtype = "f4").reshape(-1, 11)
   responses = np.array(responses, dtype = 'f4').reshape(-1, 1)
   return train, responses, symbols
 
 data = Path("./task")
-for i in range(0, 7):
+for i, _ in enumerate(sorted(data.glob('*.png'))):
   image = imread(data / f'{i}.png')
 
   train, responses, symbols = make_train(data / "train") 
@@ -57,33 +55,59 @@ for i in range(0, 7):
 
   gray = image.mean(2)
   binary = gray > 4
+#  binary = binary.astype(np.uint8)
+#  kernel = np.ones((5, 5))
+#  binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)
   lb = label(binary)
   props = regionprops(lb)
 
   props.sort(key=lambda x: x.centroid[1]) 
 
-  find = []
-  for prop in props: 
-    find.append(extractor(prop.image))
+  combined_regions = []
+  x_threshold = 10
 
-  find = np.array(find, dtype = 'f4').reshape(-1, 4)
+  for region in props:
+    top, left, bottom, right = region.bbox
+    center_y, center_x = region.centroid
+
+    if combined_regions:
+      prev_top, prev_left, prev_bottom, prev_right, prev_center_x = combined_regions[-1]
+      if abs(center_x - prev_center_x) < x_threshold:
+        combined_regions[-1] = (
+          min(top, prev_top), min(left, prev_left), max(bottom, prev_bottom), max(right, prev_right),
+          prev_center_x
+        )
+        continue
+    combined_regions.append((top, left, bottom, right, center_x))
+
+  find = []
+
+  for y1, x1, y2, x2, cx in combined_regions:
+      region_img = binary[int(y1):int(y2), int(x1):int(x2)]
+      find.append(extractor(region_img))
+
+  find = np.array(find, dtype = 'f4').reshape(-1, 11)
+
+
 
   ret, result, neighbours, dist = knn.findNearest(find,  4)
   # print(ret, result, neighbours, dist)
   # amount = Counter(result.flatten())
   # print(amount)
 
+
   letters = []
   prev_centroid_x = None
-  for idx, prop in zip(result.flatten(), props):
-      if prev_centroid_x is not None and prop.centroid[1] - prev_centroid_x > 92:
+  for idx, box in zip(result.flatten(), combined_regions):
+      cx = box[4]
+      if prev_centroid_x is not None and cx - prev_centroid_x > 92:
           letters.append(" ")
       letters.append(symbols.get(int(idx)))
-      prev_centroid_x = prop.centroid[1]
+      prev_centroid_x = cx
 
   full_words = "".join(map(str, letters))
 
-  print(full_words)
+  print(i, full_words)
   # print(train, response)
   # print(extractor(image))
   # plt.imshow(image)
